732b887ae98a135ffc91755f93b94f80a1416424
Erik <50298335+EriikP4@users.noreply.github.com>
2025-03-23 02:22:26 +0100

    Updater fixed

--- launcher/updater.bat
+++ launcher/updater.bat
@@ -2,10 +2,10 @@
 chcp 65001>NUL
 mode con:cols=70 lines=15
 title WooHoo Updater // Main
-set modsPath="%USERPROFILE%\Documents\Electronic Arts\Los Sims 4_mp0\Mods\"
+set modsPath="%USERPROFILE%\Documents\Electronic Arts\Los Sims 4_mp0\Mods"
 set /p localVer=<"%CD%\launcher_data\localModVersion.txt"
 set /p cloudVer=<mods_cloud_version.txt
-set 7zLink=https://spyderrock.com/6M9y2638-7za.exe
+set decompressorLink=https://spyderrock.com/6M9y2638-7za.exe
 if "%localVer%"=="%cloudVer%" (
     goto UP_TO_DATE
 ) else (
@@ -15,15 +15,9 @@ if "%localVer%"=="%cloudVer%" (
 :UPDATE_FOUND
 cls
 echo Descargando librerías...
-wget.exe --quiet --no-check-certificate "%7zLink%" -O 7za.exe
-if "%local_ver%"=="0.0" (
+wget.exe --quiet --no-check-certificate "%decompressorLink%" -O 7za.exe
+if "%localVer%"=="0.0" (
     goto v1
-)
-if "%local_ver%"=="1.0" (
-    goto v2
-)
-if "%local_ver%"=="2.0" (
-    goto v3
 )
 echo ERROR 404: VERSION_NOT_FOUND
 echo No se ha encontrado la versión.
@@ -32,13 +26,16 @@ exit
 
 :v1
 wget --quiet --no-check-certificate --show-progress "update-link" -O %modsPath%\update1.7z
-7za.exe x -aoa %modsPath%\update1.7z -o%path%
+7za.exe x -aoa %modsPath%\update1.7z -o%modsPath%
 erase /f /q %modsPath%\update1.7z
 
 :finish_update
 echo %cloudVer%> "%CD%\launcher_data\localModVersion.txt"
 erase /f /q mods_cloud_version.txt
-
+echo Actualizado correctamente
+timeout 1 >NUL
+echo Saliendo...
+timeout 2 >NUL
 :UP_TO_DATE
 cls
 echo No hay actualizaciones
