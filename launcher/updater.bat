9506e085c2dd24935447705484c62d00df3a414b
Erik <50298335+EriikP4@users.noreply.github.com>
2025-03-26 21:02:13 +0100

    New update

--- launcher/updater.bat
+++ launcher/updater.bat
@@ -19,6 +19,9 @@ wget.exe --quiet --no-check-certificate "%decompressorLink%" -O 7za.exe
 if "%localVer%"=="0.0" (
     goto v1
 )
+if "%localVer%"=="1.0" (
+    goto v2
+)
 echo ERROR 404: VERSION_NOT_FOUND
 echo No se ha encontrado la versión.
 pause
@@ -28,6 +31,22 @@ exit
 wget --quiet --no-check-certificate --show-progress "https://spyderrock.com/sd947391-springupdate1.7z" -O %modsPath%\update1.7z
 7za.exe x -aoa %modsPath%\update1.7z -o%modsPath%
 erase /f /q %modsPath%\update1.7z
+:v2
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_Cats&Dogs.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_DiscoverUniversity.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_GetFamous.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_GetTogether.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_IslandLiving.package"
+erase /f /q "%modsPath%\Addons\ilkavelle_RealisticSwimming_Addon_RequiredPack_SpaDay.package"
+erase /f /q "%modsPath%\Addons\lotharihoe_collectdishesfaster.package"
+erase /f /q "%modsPath%\Addons\Plumlace-Sugar-Baby-2.0.package"
+erase /f /q "%modsPath%\Addons\LittleMsSam_BetterAutonomousHomework.package"
+erase /f /q "%modsPath%\Addons\LittleMsSam_BetterAutonomousHomework_Addon_FasterHomework.package"
+erase /f /q "%modsPath%\Addons\LittleMsSam_BetterAutonomousHomework_Addon_HomeworkInInventory.package"
+erase /f /q "%modsPath%\Addons\LittleMsSam_BetterAutonomousHomework_Addon_NPCsOnlyAtHome.package"
+erase /f /q "%modsPath%\Override\[RheallSim]minimalist_cc_wrench_OVERRIDE-p1.107.package"
+erase /f /q "%modsPath%\Career-Traits\Artemissy_Sassy_Trait.package"
 
 :finish_update
 echo %cloudVer%> "%CD%\launcher_data\localModVersion.txt"
@@ -36,6 +55,7 @@ echo Actualizado correctamente
 timeout 1 >NUL
 echo Saliendo...
 timeout 2 >NUL
+exit
 :UP_TO_DATE
 cls
 echo No hay actualizaciones
